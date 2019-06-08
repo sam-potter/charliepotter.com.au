6829d0d70dfcf600812413b09cd55f5d38af1d17
Sam Potter <sam.potter@hotmail.com>
2019-06-08 11:05:10 +1000

    Finalised design and SEO

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express'),
     expressLayouts = require('express-ejs-layouts');
 
 var app = express();
-
 var isProduction = process.env.NODE_ENV === 'production';
 
 app.use(cors());
@@ -16,11 +15,11 @@ app.set('view engine', 'ejs');
 var tabs = tab => [{ name: 'Latest', active: (tab == 'Latest') }, { name: 'About', active: (tab == 'About') }, { name: 'Actor', active: (tab == 'Actor') }, { name: 'Director', active: (tab == 'Director') }];
 
 app.get('/', (req, res) => res.redirect('/about'));
+
 app.get('/latest', (req, res) => res.render('pages/latest.ejs', { tabs: tabs('Latest') }));
 app.get('/about', (req, res) => res.render('pages/about.ejs', { tabs: tabs('About') }));
 app.get('/director', (req, res) => res.render('pages/director.ejs', { tabs: tabs('Director') }));
 app.get('/actor', (req, res) => res.render('pages/actor.ejs', { tabs: tabs('Actor') }));
 app.get('/contact', (req, res) => res.render('pages/contact.ejs', { tabs: tabs() }));
-app.use((req, res) => res.status(404).render('Error'));
 
 var server = app.listen(process.env.PORT || 3000, () => console.log('Listening on port ' + server.address().port));
