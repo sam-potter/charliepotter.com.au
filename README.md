c0d65a1dc1842a79eabc45501bdb922305329307
Sam Potter <sam.potter@hotmail.com>
2019-06-13 18:06:44 +1000

    Dark redesign

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 # Project goal and information
 This is the git repo to track the designs, and re-designs of my brother's personal portfolio website.
 
+To view the site live visit: <a href="http://www.charliepotter.com.au/">charliepotter.com.au</a>
+
 # Author
 Sam Potter, sam.potter@hotmail.com
--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-var express = require('express'),
-	cors = require('cors'),
-	helmet = require('helmet'),
-    expressLayouts = require('express-ejs-layouts');
+const express = require('express');
+// const cors = require('cors');
+// const helmet = require('helmet');
+const http = require('http');
+const reload = require('reload');
+const expressLayouts = require('express-ejs-layouts');
 
-var app = express();
-var isProduction = process.env.NODE_ENV === 'production';
+const app = express();
+// const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(cors());
-app.use(helmet());
+// app.use(cors());
+// app.use(helmet());
 app.use(expressLayouts);
 app.use('/assets', express.static('assets'));
 app.set('view engine', 'ejs');
 
-var tabs = tab => [{ name: 'Latest', active: (tab == 'Latest') }, { name: 'About', active: (tab == 'About') }, { name: 'Actor', active: (tab == 'Actor') }, { name: 'Director', active: (tab == 'Director') }];
+const tabs = tab => [{ name: 'Latest', active: (tab == 'Latest') }, { name: 'About', active: (tab == 'About') }, { name: 'Actor', active: (tab == 'Actor') }, { name: 'Director', active: (tab == 'Director') }];
 
 app.get('/', (req, res) => res.redirect('/about'));
-
 app.get('/latest', (req, res) => res.render('pages/latest.ejs', { tabs: tabs('Latest') }));
 app.get('/about', (req, res) => res.render('pages/about.ejs', { tabs: tabs('About') }));
 app.get('/director', (req, res) => res.render('pages/director.ejs', { tabs: tabs('Director') }));
 app.get('/actor', (req, res) => res.render('pages/actor.ejs', { tabs: tabs('Actor') }));
 app.get('/contact', (req, res) => res.render('pages/contact.ejs', { tabs: tabs() }));
 
-var server = app.listen(process.env.PORT || 3000, () => console.log('Listening on port ' + server.address().port));
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000, () => console.log('Listening on port ' + server.address().port));
+reload(app);
