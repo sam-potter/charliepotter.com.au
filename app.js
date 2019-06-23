fc11b7dcad5653bda8426878cda3b64ece0c145a
Sam Potter <sam.potter@hotmail.com>
2019-06-23 14:14:36 +1000

    Added Barba.js transitions + design changes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
-// const cors = require('cors');
-// const helmet = require('helmet');
-const http = require('http');
-const reload = require('reload');
+const cors = require('cors');
+const helmet = require('helmet');
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
-// const isProduction = process.env.NODE_ENV === 'production';
 
-// app.use(cors());
-// app.use(helmet());
+app.use(cors());
+app.use(helmet());
 app.use(expressLayouts);
 app.use('/assets', express.static('assets'));
 app.set('view engine', 'ejs');
@@ -23,6 +20,4 @@ app.get('/director', (req, res) => res.render('pages/director.ejs', { tabs: tabs
 app.get('/actor', (req, res) => res.render('pages/actor.ejs', { tabs: tabs('Actor') }));
 app.get('/contact', (req, res) => res.render('pages/contact.ejs', { tabs: tabs() }));
 
-const server = http.createServer(app);
-server.listen(process.env.PORT || 3000, () => console.log('Listening on port ' + server.address().port));
-reload(app);
+app.listen(3000, () => console.log('Listening on port 3000'));
