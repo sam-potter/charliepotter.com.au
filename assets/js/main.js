506df8665f6ce6dfc7cb90e9ac18948f040f28ff
Sam Potter <sam.potter@hotmail.com>
2019-06-23 16:08:55 +1000

    Added page transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 var _toggleMobileNav = () => document.getElementById('mobile-nav').classList.toggle('active');
-window.onload = () => barba.init();
+
+window.onload = () => {
+    barba.init();
+
+    barba.hooks.before(data => {
+        return new Promise(resolve => {
+        	var el = data.current.container.querySelector('#wrapper');
+        	var animation = { css: { opacity: 0, transform: 'translateY(-20px)' } };
+        	TweenMax.to(el, 0.25, animation).eventCallback('onComplete', resolve)
+        });
+    });
+
+    barba.hooks.beforeEnter(data => {
+        var el = data.next.container.querySelector('#wrapper');
+       	var animation = { css: { opacity: 0, transform: 'translateY(20px)' } };
+        TweenMax.from(el, 0.25, animation);
+    });
+}
